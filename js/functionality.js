27739943adf329286bb9b57079f12dfaa57a84c9
aaronrs2002 <aaronrs2002@yahoo.com>
2022-08-03 07:02:55 -0700

    Add files via upload
    first commit

---
+++ js/functionality.js
@@ -0,0 +1,40 @@
+function generate() {
+    return Math.floor(Math.random() * icons.length);
+}
+
+let random = null;
+let iconObj = [];
+function getRandom() {
+    if (random) {
+        iconObj = [];
+        let iconTargetHTML = "";
+        for (let i = 0; i < 3; i++) {
+            const number = generate();
+            iconObj.push(icons[number].value);
+            iconTargetHTML = iconTargetHTML + "<div data-icon='" + icons[number].title + "'></div>";
+        }
+        document.getElementById("iconTarget").innerHTML = iconTargetHTML;
+    } else {
+        clearInterval(random);
+        console.log("random is false;");
+        return false;
+    }
+
+}
+
+function checkRandom(status) {
+    if (status == true) {
+        random = setInterval(getRandom, 100);
+    } else {
+        let iconSum = 0;
+        for (let i = 0; i < iconObj.length; i++) {
+            iconSum = iconSum + Number(iconObj[i]);
+        }
+        document.getElementById("iconSum").innerHTML = iconSum;
+        clearInterval(random, null);
+        random = null;
+        return false;
+    }
+
+}
+
---
+++ js/icons.js
@@ -0,0 +1,16 @@
+const icons = [
+    { title: "cherry", value: 50 },
+    { title: "lemon", value: 50 },
+    { title: "watermelon", value: 50 },
+    { title: "bell", value: 0 },
+    { title: "bar", value: -250 },
+    { title: "heart", value: 100 },
+    { title: "purple", value: 50 },
+    { title: "clover", value: 100 },
+    { title: "orange", value: 50 },
+    { title: "horseshoe", value: 150 },
+    { title: "grapes", value: 50 },
+    { title: "apple", value: 50 },
+    { title: "diamond", value: 250 },
+    { title: "dollars", value: 100 }
+];
